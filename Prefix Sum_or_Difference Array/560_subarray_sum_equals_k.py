ef41aac6a5fbbb74604a00ad507730c305592506
Daksh <Daksh07652@gmail.com>
2026-08-07 21:32:56 +0530

    problem 560

---
+++ Prefix Sum_or_Difference Array/560_subarray_sum_equals_k.py
@@ -0,0 +1,10 @@
+class Solutions:
+    def subarray_sum_equals_k(self , nums,k):
+        count = 0
+        prefix_sum = 0
+        seen ={0:1}
+        for num in nums:
+            prefix_sum += num
+            count += seen.get(prefix_sum - k , 0)
+            seen[prefix_sum] = seen.get(prefix_sum , 0) +1
+        return count
