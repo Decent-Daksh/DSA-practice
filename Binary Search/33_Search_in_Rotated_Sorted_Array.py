e6eb4e45a58f362aabfe5137e18a6fa44c3f4766
Daksh <Daksh07652@gmail.com>
2026-09-16 09:34:24 +0530

    problem 33: seearch in rotated sorted array

--- Binary Search/33_Search_in_Rotated_Sorted_Array.py
+++ Binary Search/33_Search_in_Rotated_Sorted_Array.py
@@ -1,4 +1,19 @@
 from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        l , r = 0 , len(nums)-1
+        while l<=r:
+            mid = l +(r-l)//2
+            if nums[mid] == target:
+                return mid
+            if nums[l]<= nums[mid]:
+                if nums[l]<=target<nums[mid]:
+                    right = mid -1
+                else:
+                    left = mid +1
+            else:
+                if nums[mid]< target <= nums[r]:
+                    left = mid +1
+                else:
+                    right = mid - 1
+        return -1
